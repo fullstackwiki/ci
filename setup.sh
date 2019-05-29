f9a3f78f47b30eb4c9b481620c0ed3340409523f
Austin Wright <aaa@bzfx.net>
2019-05-29 12:54:01 -0700

    Adjust location of dive-publish-aws

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ mkdir "$2.work/web/ci"
 pushd "$2.work"
 FORCE_COLOR=2 bash ../setup-test.sh "$1" "$2" 2>&1 | tee "web/ci/$2.log"
 cat "web/ci/$2.log" | node ../format-log.js > "web/ci/$2.xml"
-AWS_PROFILE=fullstack-wiki node ../../dive-publish-aws/publish.js --base http://fullstack.wiki/ --bucket fullstack.wiki app.js
+node ../dive-publish-aws/publish.js --base http://fullstack.wiki/ --bucket fullstack.wiki app.js
 popd
 rm -rf $2.work
