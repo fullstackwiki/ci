dea0e9b7a5fcead7c11ec7749a3a82b52d3793c8
Austin Wright <aaa@bzfx.net>
2020-08-08 00:26:36 -0700

    Update title in format-log.js output

--- format-log.js
+++ format-log.js
@@ -1,5 +1,8 @@
+"use strict";
 
 var convert = new (require('ansi-to-html'))({
+	fg: '#000',
+	bg: '#FFF',
 	stream: true,
 	escapeXML: true,
 });
@@ -10,12 +13,13 @@ process.stdout.write('<!DOCTYPE html>\n');
 process.stdout.write('<html xmlns="http://www.w3.org/1999/xhtml" xmlns:w="tag:fullstack.wiki,2018:ns/" lang="en" dir="ltr">');
 process.stdout.write('	<head>');
 process.stdout.write('		<meta charset="UTF-8" />');
-process.stdout.write('		<title>Welcome to Fullstack.wiki</title>');
+process.stdout.write('		<title>CI task</title>');
+process.stdout.write('		<meta name="robots" content="noindex" />');
 process.stdout.write('		<meta name="description" content="Fullstack.wiki homepage" />');
 process.stdout.write('	</head>');
 process.stdout.write('	<body>');
 process.stdout.write('		<main>');
-process.stdout.write('			<h1>Welcome to Fullstack.wiki</h1>');
+process.stdout.write('			<h1>CI task</h1>');
 process.stdout.write('			<pre>');
 
 process.stdin.on('data', function (chunk) {
